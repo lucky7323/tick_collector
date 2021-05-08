a86333a4fc56abb6159d3ae597586d7f6577f73a
eunho-lee <eunho.ambition@gmail.com>
2021-05-08 10:22:49 +0000

    use loguru logger

--- collector.py
+++ collector.py
@@ -1,15 +1,12 @@
 from binance.client import Client
 from binance.websockets import BinanceSocketManager
 import argparse
-import logging.handlers
+from loguru import logger
 
-file_name = "ticker.csv"
-logger = logging.getLogger('tick')
-logger.setLevel(logging.INFO)
-handler = logging.handlers.RotatingFileHandler(file_name, maxBytes=10**9, backupCount=20)
-formatter = logging.Formatter("%(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+
+def set_logger(log_dir, ticker):
+    logger.remove()
+    logger.add(f"{log_dir}{ticker}" + "_{time}.csv", format="{message}", rotation="2 GB", compression="zip")
 
 
 def process_message(msg):
@@ -20,7 +17,11 @@ def process_message(msg):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--symbol", required=True, type=str, help="symbol {base asset}{quote asset} (ex. 'BTCUSDT')")
-    symbol = parser.parse_args().symbol.upper()
+    parser.add_argument("--log_dir", type=str, help="log directory path", default="./logs/")
+    args = parser.parse_args()
+    symbol = args.symbol.upper()
+
+    set_logger(args.log_dir, symbol)
     client = Client("", "")
     bm = BinanceSocketManager(client)
     conn_key = bm.start_trade_socket(symbol, process_message)
