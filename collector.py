51a703817d8beaaaab785132af5c292185620e02
eunho-lee <eunho.ambition@gmail.com>
2021-05-11 14:20:07 +0000

    feat: support future market data

--- collector.py
+++ collector.py
@@ -1,32 +1,53 @@
-from binance.client import Client
-from binance.websockets import BinanceSocketManager
+from binance import AsyncClient, DepthCacheManager, BinanceSocketManager
+import asyncio
 import argparse
 from loguru import logger
 
 
-def set_logger(data_dir, ticker):
+def set_logger(data_dir, ticker, future: bool):
+    prefix = "future_" if future else ""
     logger.remove()
-    logger.add(f"{data_dir}{ticker}" + "_{time}.csv", format="{message}", rotation="2 GB", compression="zip",
+    logger.add(f"{data_dir}{prefix}{ticker}" + "_{time}.csv", format="{message}", rotation="1 GB", compression="zip",
                filter=lambda record: record["extra"]["task"] == "data")
 
 
-def process_message(msg):
-    msg = f"{msg['e']},{msg['E']},{msg['s']},{msg['t']},{msg['p']},{msg['q']},{msg['b']},{msg['a']},{msg['T']},{msg['m']}"
+def process_message(msg, future: bool):
+    if future:
+        msg = f"{msg['e']},{msg['E']},{msg['s']},{msg['a']},{msg['p']},{msg['q']},{msg['f']},{msg['l']},{msg['T']},{msg['m']}"
+    else:
+        msg = f"{msg['e']},{msg['E']},{msg['s']},{msg['t']},{msg['p']},{msg['q']},{msg['b']},{msg['a']},{msg['T']},{msg['m']}"
     data_logger = logger.bind(task="data")
     data_logger.info(msg)
+
+
+async def main(symbol: str, future: bool):
+    market_type = "future" if future else "spot"
+    print(f'Started Collecting Tick Data of {symbol}...({market_type} market)')
+    client = await AsyncClient.create()
+    bsm = BinanceSocketManager(client)
+
+    if future:
+        async with bsm.aggtrade_futures_socket(symbol) as socket:
+            while True:
+                res = await socket.recv()
+                process_message(res['data'], future)
+    else:
+        async with bsm.trade_socket(symbol) as socket:
+            while True:
+                res = await socket.recv()
+                process_message(res, future)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--symbol", required=True, type=str, help="symbol {base asset}{quote asset} (ex. 'BTCUSDT')")
     parser.add_argument("--data_dir", type=str, help="data directory path", default="./data/")
+    parser.add_argument("--future", action='store_true', help="for future market data")
     args = parser.parse_args()
     symbol = args.symbol.upper()
 
-    set_logger(args.data_dir, symbol)
-    client = Client("", "")
-    bm = BinanceSocketManager(client)
-    conn_key = bm.start_trade_socket(symbol, process_message)
-    bm.start()
-    print(f'Started Collecting Tick Data of {symbol}...')
+    set_logger(args.data_dir, symbol, args.future)
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(symbol, args.future))
 
