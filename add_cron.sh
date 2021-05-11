f5b142c8c5b5de889bf0f8165cf36a9abaaf7125
eunho-lee <eunho.ambition@gmail.com>
2021-05-11 14:39:22 +0000

    feat: enable telegram-bot option as default

--- add_cron.sh
+++ add_cron.sh
@@ -7,7 +7,8 @@ CURRENT_PYTHON=`which python3`
 CRON_CMD="upload_s3.py --log_dir $BASE_DIR/logs/ \
   --data_dir $BASE_DIR/data/ \
   --config $BASE_DIR/config.json \
-  --rm"
+  --rm \
+  --telegram"
 
 (crontab -l 2>/dev/null; echo "0 0 * * * cd $BASE_DIR && $CURRENT_PYTHON $CRON_CMD") | crontab -
 
